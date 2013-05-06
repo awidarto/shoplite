3b41032fa53cb58cb367ffbe1e1b6e9a2fd64c4a
taufiqridha <taufiq.ridha@gmail.com>
2013-05-06 19:39:54 +0700

    tarikkkk

--- application/views/shop/home.blade.php
+++ application/views/shop/home.blade.php
@@ -3,19 +3,79 @@
 @section('content')
 <div class="row">
 	<div class="span4">
-		<img src="{{ URL::base() }}/storage/mm/mm01.jpg" alt="mm01" class="mixmatch"  />
+		<img src="{{ URL::base() }}/images/mm/mm01.jpg" alt="mm01" class="mixmatch"  />
 		<br />
-		<img src="{{ URL::base() }}/storage/pow/pow01.jpg" alt="mm01" />
+		<img src="{{ URL::base() }}/images/pow/pow01.jpg" alt="mm01" />
 	</div>
 	<div class="span4">
-		<img src="{{ URL::base() }}/storage/mm/mm02.jpg" alt="mm01" class="mixmatch"  />
+		<img src="{{ URL::base() }}/images/mm/mm02.jpg" alt="mm01" class="mixmatch"  />
 		<br />
-		<img src="{{ URL::base() }}/storage/otb/otb01.jpg" alt="mm01" />
+		<img src="{{ URL::base() }}/images/otb/otb01.jpg" alt="mm01" />
 	</div>
 	<div class="span4">
-		<img src="{{ URL::base() }}/storage/mm/mm03.jpg" alt="mm01" class="mixmatch" />
+		<img src="{{ URL::base() }}/images/mm/mm03.jpg" alt="mm01" class="mixmatch" />
 		<br />
-		<img src="{{ URL::base() }}/storage/kind/kin01.jpg" alt="mm01" />
+		<img src="{{ URL::base() }}/images/kind/kin01.jpg" alt="mm01" />
+	</div>
+
+	<div class="clear"></div>
+	<div class="articlesetionhome">
+		<div class="span4">
+			<div class="articlehome">
+				<h2 class="titlehome">Mix and Match 01</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sollicitudin fringilla dolor, eu fringilla libero iaculis in. Nunc sodales laoreet purus, et pharetra orci venenatis ut. Vestibulum nisi arcu, porta ut aliquet id, accumsan ut dui. Donec est tortor, tempus vitae porttitor eu, ullamcorper sed nulla. Pellentesque nisl neque, sollicitudin vel pretium nec, dapibus id mauris. Curabitur ut libero a nibh congue ullamcorper vitae sit amet dui. Nam accumsan, ligula ut auctor pharetra, ligula diam egestas turpis, id scelerisque lorem risus vitae ante. Sed vulputate libero eu neque vestibulum suscipit. Praesent nec ultrices elit. Aliquam ultricies felis nec neque auctor malesuada vitae quis quam. 
+					
+				</p>
+				<a class="readmore">readmore ></a>
+			</div>
+			<div class="articlehome">
+				<h2 class="titlehome">Mix and Match 02</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sollicitudin fringilla dolor, eu fringilla libero iaculis in. Nunc sodales laoreet purus, et pharetra orci venenatis ut. Vestibulum nisi arcu, porta ut aliquet id, accumsan ut dui. Donec est tortor, tempus vitae porttitor eu, ullamcorper sed nulla. Pellentesque nisl neque, sollicitudin vel pretium nec, dapibus id mauris. Curabitur ut libero a nibh congue ullamcorper vitae sit amet dui. Nam accumsan, ligula ut auctor pharetra, ligula diam egestas turpis, id scelerisque lorem risus vitae ante. Sed vulputate libero eu neque vestibulum suscipit. Praesent nec ultrices elit. Aliquam ultricies felis nec neque auctor malesuada vitae quis quam. 
+					
+				</p>
+				<a class="readmore">readmore ></a>
+			</div>
+		</div>
+
+		<div class="span4">
+			<div class="articlehome">
+				<h2 class="titlehome">PICK OF THE WEEK: March 1 2013</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sollicitudin fringilla dolor, eu fringilla libero iaculis in. Nunc sodales laoreet purus, et pharetra orci venenatis ut. Vestibulum nisi arcu, porta ut aliquet id, accumsan ut dui. Donec est tortor, tempus vitae porttitor eu, ullamcorper sed nulla. Pellentesque nisl neque, sollicitudin vel pretium nec, dapibus id mauris. Curabitur ut libero a nibh congue ullamcorper vitae sit amet dui. Nam accumsan, ligula ut auctor pharetra, ligula diam egestas turpis, id scelerisque lorem risus vitae ante. Sed vulputate libero eu neque vestibulum suscipit. Praesent nec ultrices elit. Aliquam ultricies felis nec neque auctor malesuada vitae quis quam. 
+					
+				</p>
+				<a class="readmore">readmore ></a>
+			</div>
+			<div class="articlehome">
+				<h2 class="titlehome">PICK OF THE WEEK: February 21 2013</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sollicitudin fringilla dolor, eu fringilla libero iaculis in. Nunc sodales laoreet purus, et pharetra orci venenatis ut. Vestibulum nisi arcu, porta ut aliquet id, accumsan ut dui. Donec est tortor, tempus vitae porttitor eu, ullamcorper sed nulla. Pellentesque nisl neque, sollicitudin vel pretium nec, dapibus id mauris. Curabitur ut libero a nibh congue ullamcorper vitae sit amet dui. Nam accumsan, ligula ut auctor pharetra, ligula diam egestas turpis, id scelerisque lorem risus vitae ante. Sed vulputate libero eu neque vestibulum suscipit. Praesent nec ultrices elit. Aliquam ultricies felis nec neque auctor malesuada vitae quis quam.
+					
+				</p>
+				<a class="readmore">readmore ></a>
+			</div>
+		</div>
+
+		<div class="span4">
+			<div class="articlehome">
+				<h2 class="titlehome">ONE OF A KIND</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sollicitudin fringilla dolor, eu fringilla libero iaculis in. Nunc sodales laoreet purus, et pharetra orci venenatis ut. Vestibulum nisi arcu, porta ut aliquet id, accumsan ut dui. Donec est tortor, tempus vitae porttitor eu, ullamcorper sed nulla. Pellentesque nisl neque, sollicitudin vel pretium nec, dapibus id mauris. Curabitur ut libero a nibh congue ullamcorper vitae sit amet dui. Nam accumsan, ligula ut auctor pharetra, ligula diam egestas turpis, id scelerisque lorem risus vitae ante. Sed vulputate libero eu neque vestibulum suscipit. Praesent nec ultrices elit. Aliquam ultricies felis nec neque auctor malesuada vitae quis quam.
+					
+				</p>
+				
+			</div>
+			<div class="articlehome">
+				<h2 class="titlehome">FASHION TIPS OF THE WEEK</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sollicitudin fringilla dolor, eu fringilla libero iaculis in. Nunc sodales laoreet purus, et pharetra orci venenatis ut. Vestibulum nisi arcu, porta ut aliquet id, accumsan ut dui. Donec est tortor, tempus vitae porttitor eu, ullamcorper sed nulla. Pellentesque nisl neque, sollicitudin vel pretium nec, dapibus id mauris. Curabitur ut libero a nibh congue ullamcorper vitae sit amet dui. Nam accumsan, ligula ut auctor pharetra, ligula diam egestas turpis, id scelerisque lorem risus vitae ante. Sed vulputate libero eu neque vestibulum suscipit. Praesent nec ultrices elit. Aliquam ultricies felis nec neque auctor malesuada vitae quis quam.
+					
+				</p>
+				
+			</div>
+		</div>
 	</div>
 
 </div>
