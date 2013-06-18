119b7256acec055e46975b160c4522e9a7044f37
Andy Awidarto <andy.awidarto@gmail.com>
2013-06-18 14:16:33 +0700

    check out commit

--- application/views/shop/commit.blade.php
+++ application/views/shop/commit.blade.php
@@ -3,108 +3,153 @@
 @section('content')
 <div class="row">
     {{-- print_r($cart['items'])}}
-    <div class="span12">
-        <h3>purchase order</h3>
-        <table class='dataTable' id="shoppingcart">
-        <thead>
+  <div class="span12">
+    <h3>Check Out Success</h3>
+    <p>
+        Thank you for shopping at Peach To Black, feel free to go back into our catalog again and shop for more.
+    </p>
+    <p>
+        Your payment confirmation code is : <span class="big-code">{{ $cart['confirmationCode']}}</span>
+    </p>
+    <p>
+        If you've made your transfer payment, kindly confirm your payment {{ HTML::link('shop/confirm', 'here') }}, using above code, so we can proceed with the delivery. Have a nice day ! 
+    </p>
+
+    <table class='dataTable' id="shoppingcart">
+      <thead>
         <tr class="headshoppingcart">
-            <th class="span2"></th>
-            <th class="span4">ITEM DESCRIPTION</th>
-            <th class="span1">SIZE / COLOR / QTY</th>
-            <th class="span2">UNIT PRICE</th>
-            <th class="span2">PRICE TOTAL</th>
+          <th class="span2"></th>
+          <th class="span4">ITEM DESCRIPTION</th>
+          <th class="span1">SIZE</th>
+          <th class="span1">COLOR</th>
+          <th class="span1">QTY</th>
+          <th class="span2">UNIT PRICE</th>
+          <th class="span2">PRICE TOTAL</th>
         </tr>
-        </thead>
-            <tbody>
-                <?php
-                    $totalPrice = 0;
-                ?>
-                @foreach($cart['items'] as $key=>$val)
+      </thead>
+      <tbody>
 
-                  <?php
-                    $i = $products[$key]['defaultpic'];
+      <?php
 
-                    $product_prefix = $key;
+      $totalPrice = 0;
 
-                  ?>
+      //print_r($cart);
 
-                  <tr>
-                    <td class="span2 image">
-                        @if(file_exists(realpath('public/storage/products/'.$key).'/sm_pic0'.$i.'.jpg'))
-                            {{ HTML::image(URL::base().'/storage/products/'.$key.'/sm_pic0'.$i.'.jpg?'.time(), 'sm_pic0'.$i.'.jpg', array('id' => $key)) }}
-                        @endif
-                    </td>
-                    <td class="span4"><h5>{{ $products[$key]['name'];}}</h5>{{ $products[$key]['description'];}}</td>
-                    <td class="span2">
-                      <table class="var-box">
-                        <?php $qty = 0;?>
-                        @foreach($val as $k=>$v)
-                          <tr>
-                            <td>
-                              {{ $v['size'] }}
-                            </td>
-                            <td>
-                              <span class="color-chip" style="background-color: {{ $v['color'] }}; ">&nbsp;</span>
-                            </td>
-                            <td>
-                              <?php $qty += $postdata[$product_prefix.'_'.$k.'_qty'];?>
-                              {{  $postdata[$product_prefix.'_'.$k.'_qty'] }}
-                              {{ Form::hidden($k.'_qty',$v['ordered'],array('class'=>'qty-box')) }}<br />
-                            </td>
-                          </tr>
-                        @endforeach
-                      </table>
-                    </td>
-                    <td class="span2 price">{{ $products[$key]['priceCurrency'].' '.number_format($postdata[$key.'_retailPrice'],2,',','.') ;}}</td>
-                    <td class="span1 price">{{ $products[$key]['priceCurrency'].' '.number_format($postdata[$key.'_subTotalPrice'],2,',','.') ;}}</td>
-                  </tr>
+      ?>
+      @foreach($cart['items'] as $key=>$val)
 
-                  <?php
-                    $totalPrice += $qty * (double) $products[$key]['retailPrice'];
-                  ?>
-                @endforeach
-                <tr>
-                    <td class="span2">PAYMENT METHOD</td>
-                    <td class="span4">SHIPPING METHOD</td>
-                    <td class="span1"></td>
-                    <td class="span2 currency-display">SUB TOTAL</td>
-                    <td class="span2 currency-display">{{ $products[$key]['priceCurrency'].' '.number_format($postdata['totalPrice'],2,',','.') ;}}</td>
-                </tr>
-                <tr>
-                    <td class="span2">
-                        @if($postdata['paymentmethod'] == 'mandiri')
-                            <img src="{{ URL::base() }}/images/mandiri.png">
-                        @elseif($postdata['paymentmethod'] == 'bca')
-                            <img src="{{ URL::base() }}/images/bca.png">
-                        @endif
-                    </td>
-                    <td class="span4">
-                        @if($postdata['shippingmethod'] == 'jex')
-                            <img src="{{ URL::base() }}/images/jexcod.png">
-                        @elseif( $postdata['shippingmethod'] == 'jne')
-                            <img src="{{ URL::base() }}/images/jne.png">
-                        @elseif($postdata['shippingmethod'] == 'gojek')
-                            <img src="{{ URL::base() }}/images/gojek.png">
-                        @endif
-                    </td>
-                    <td class="span1"></td>
-                    <td class="span2 currency-display">SHIPPING FEE</td>
-                    <td class="span2 currency-display">{{ $products[$key]['priceCurrency'].' '.number_format($postdata['shippingFee'],2,',','.') ;}}</td>
-                </tr>
-                <tr>
-                    <td colspan="3"></td>
-                    <td class="span2 currency-display">TOTAL PRICE</td>
-                    <td class="span2 currency-display">{{ $products[$key]['priceCurrency'].' '.number_format($postdata['totalDue'],2,',','.') ;}}</td>
-                </tr>
-                <tr>
-                    <td colspan="4"></td>
-                    <td class="span2 currency-display">
-                        <a class="btn primary" href="{{ URL::base();}}"><i class="icon-shopping"></i> Shop for more</a></p>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+          <?php
+            $i = $products[$key]['defaultpic'];
+            $product_prefix = $key;
+          ?>
+
+          @foreach($val as $k=>$v)
+            <?php
+              $kx = str_replace('#', '', $k);
+            ?>
+          <tr>
+            <td class="span2 image">
+                @if(file_exists(realpath('public/storage/products/'.$key).'/sm_pic0'.$i.'.jpg'))
+                    {{ HTML::image(URL::base().'/storage/products/'.$key.'/sm_pic0'.$i.'.jpg?'.time(), 'sm_pic0'.$i.'.jpg', array('id' => $key)) }}
+                @endif
+            </td>
+            <td class="span4"><h5>{{ $products[$key]['name'];}}</h5>{{ $products[$key]['description'];}}</td>
+
+            <?php $qty = 0;?>
+              <?php
+
+                  $vx = explode('_',$k);
+                  $size = $vx[0];
+                  $color = $vx[1];
+
+              ?>
+            <td class="span1 central" >
+              {{ $size }}
+            </td>
+            <td class="span1 central" >
+              <span class="color-chip" style="background-color: {{ $color }}; ">&nbsp;</span>
+            </td>
+            <td class="span2 central">
+              {{  $cart['items'][$product_prefix][$k]['actual'] }}
+              {{ Form::hidden($product_prefix.'_'.$k.'_qty',$cart['items'][$product_prefix][$k]['actual'],array('class'=>'qty-box')) }}<br />
+            </td>
+
+            <td class="span2 price">
+              {{ $cart['prices'][$product_prefix][$k]['unit_price_fmt'] ;}}
+              <input type="hidden" name="{{$key}}_retailPrice" value="{{$cart['prices'][$product_prefix][$k]['unit_price']}}" />
+            </td>
+            <td class="span2 price">
+              {{ $cart['prices'][$product_prefix][$k]['sub_total_price_fmt'] ;}}
+          </tr>
+
+          @endforeach
+
+          <?php
+            $totalPrice += $qty * (double) $products[$key]['retailPrice'];
+          ?>
+      @endforeach
+          <tr>
+            <td colspan="5"></td>
+            <td class="span2 price">
+              <h4 class="titleselectbox">sub-total</h4>
+            </td>
+            <td class="span2 price">
+              {{ $cart['prices']['total_due_fmt'] }}
+            </td>
+          </tr>
+
+          <tr>
+            <td colspan="5"></td>
+            <td class="span2 price">
+              <h4 class="titleselectbox">shipping</h4>
+            </td>
+            <td class="span2 price">
+              {{ $cart['prices']['shipping_fmt'] }}
+            </td>
+          </tr>
+
+          <tr>
+            <td colspan="5"></td>
+            <td class="span2 price">
+              <h4 class="titleselectbox">total</h4>
+            </td>
+            <td class="span2 price">
+              {{ $cart['prices']['total_due_fmt'] }}
+            </td>
+          </tr>
+
+      </tbody>
+    </table>
+    <div class="clear"></div>
+    <div class="paymentmethod span12">
+      <div class="method1 span3">
+        <h4>payment method</p>  
+        @if($postdata['paymentmethod'] == 'mandiri')
+          <img src="{{ URL::base() }}/images/mandiri.png">
+        @elseif($postdata['paymentmethod'] == 'bca')
+          <img src="{{ URL::base() }}/images/bca.png">
+        @endif
+        <input type="hidden" value="{{$postdata['paymentmethod']}}" name="paymentmethod">
+      </div>
+
+      <div class="method2 span3">
+        <h4>shipping method</p>
+        @if($postdata['shippingmethod'] == 'jex')
+          <img src="{{ URL::base() }}/images/jexcod.png">
+        @elseif( $postdata['shippingmethod'] == 'jne')
+          <img src="{{ URL::base() }}/images/jne.png">
+        @elseif($postdata['shippingmethod'] == 'gojek')
+          <img src="{{ URL::base() }}/images/gojek.png">
+        @endif
+        <input type="hidden" value="{{$postdata['shippingmethod']}}" name="shippingmethod">
+      </div>
+
+      <div class="method3 span5">
+
+      </div>
+      
     </div>
+
 </div>
 
 <style type="text/css">
