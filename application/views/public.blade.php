a54df12e10f426e3e477da7fc29ebfd1cfe68f35
Andy Awidarto <andy.awidarto@gmail.com>
2013-07-09 17:43:00 +0700

    detail fix

--- application/views/public.blade.php
+++ application/views/public.blade.php
@@ -69,8 +69,8 @@
     <div id="footer">
       <div class="container">
         <p class="social-footer">
-          <a href="http://www.facebook.com/peachtoblack"><i class="icon-facebook-2"></i></a>
-          <a href="http://www.twitter.com/peachtoblack"><i class="icon-twitter-2"></i></a>
+          <a href="http://www.facebook.com/peachtoblack" target="_blank" ><i class="icon-facebook-2"></i></a>
+          <a href="http://www.twitter.com/peachtoblack" target="_blank" ><i class="icon-twitter-2"></i></a>
       <!--
           <i class="icon-pinterest"></i>
           <i class="icon-google-plus-3"></i>
